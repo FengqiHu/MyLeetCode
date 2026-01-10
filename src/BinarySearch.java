893129b8bce3bf442478509dadb28359f08fdb6d
FengqiHu <louishurris@gmail.com>
2026-01-09 21:04:47 -0500

    Solve Q74, 100%, memory 26.15%

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -7,7 +7,7 @@ public class BinarySearch {
 
     /**
      * 35. Search Insert Position - Easy
-     *
+     * @Date 01/09/2026
      * @param nums
      * @param target
      * @return
@@ -32,4 +32,33 @@ public class BinarySearch {
 
         }
     }
+
+    /**
+     * 74. Search a 2D Matrix - Medium
+     * @Date 01/09/2026
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int start = 0, end = matrix.length - 1, targetRow = 0;
+
+        for (int i = matrix.length - 1; i >0 ; i--) {
+            if (matrix[i][0]< target){
+                targetRow = i;
+                break;
+            }else if (matrix[i][0] == target){
+                return true;
+            }
+        }
+
+        for (int i = 0; i < matrix[targetRow].length; i++) {
+            if (matrix[targetRow][i] == target) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
