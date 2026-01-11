b7f06b995eefdba76e352417cfb9373c70deddc9
FengqiHu <louishurris@gmail.com>
2026-01-11 16:52:17 -0500

    Solve Q49, 42.85%, memory 65.17%

--- src/StringSet.java
+++ src/StringSet.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class StringSet {
     /**
@@ -264,6 +261,28 @@ public class StringSet {
         return true;
     }
 
+    /**
+     * 49. Group Anagrams - Medium
+     * @Date 01/11/2026
+     * @param strs
+     * @return
+     */
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] chars = s.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
 
 
     public static void main(String[] args) {
