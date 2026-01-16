c48838a1693e45a478cc81e44295bf647976ee8e
FengqiHu <louishurris@gmail.com>
2026-01-15 22:43:24 -0500

    Solve Q63, 100%, memory 6.24%

--- src/GreedyAlgo.java
+++ src/GreedyAlgo.java
@@ -177,6 +177,41 @@ public class GreedyAlgo {
         return path[m - 1][n - 1];
 
     }
+    /**
+     * 63. Unique Paths II - medium
+     * @Date - 01/15/2026
+     * @return
+     */
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int path[][] = new int[m][n];
+        path[0][0] = 1;
+
+        if (obstacleGrid[0][0] == 1)
+            return 0;
+        for (int i = 1; i < m; i++) {
+            if (obstacleGrid[i][0] == 1)
+                break;
+            else
+                path[i][0] = 1;
+        }
+        for (int i = 1; i < n; i++) {
+            if (obstacleGrid[0][i] == 1)
+                break;
+            else
+                path[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0)
+                    path[i][j] = path[i - 1][j] + path[i][j - 1];
+            }
+
+        }
+        return path[m - 1][n - 1];
+
+    }
 
 
     public static void main(String[] args) {
