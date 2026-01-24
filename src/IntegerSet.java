f6addb26d84f8251ad7e281a6ab83b163c6c6d8b
FengqiHu <louishurris@gmail.com>
2026-01-24 00:31:52 -0500

    Solve Q4, 25.86%, memory 63.70%

--- src/IntegerSet.java
+++ src/IntegerSet.java
@@ -504,9 +504,35 @@ public class IntegerSet {
         }
     }
 
+    /**
+     * 4. Median of Two Sorted Arrays - Hard
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     * @Date - 01/24/2026
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int num[] = new int[nums1.length + nums2.length];
+
+        for (int i = 0; i < nums1.length; i++) {
+            num[i] = nums1[i];
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            num[nums1.length + i] = nums2[i];
+        }
+
+        Arrays.sort(num);
+        if (num.length % 2 == 0) {
+            return (num[num.length / 2] + num[num.length / 2 - 1]) / 2.0;
+        } else {
+            return num[num.length / 2];
+        }
+    }
+
     public static void main(String[] args) {
-        int a = 1000000000+1000000000+1000000000+1000000000;
-        if (a>Integer.MAX_VALUE){
+        int a = 1000000000 + 1000000000 + 1000000000 + 1000000000;
+        if (a > Integer.MAX_VALUE) {
             System.out.println("yes");
         }
         System.out.println(Integer.MAX_VALUE);
