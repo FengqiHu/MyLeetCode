8e7391cbdd4a032080009305f3b6112eb9c30300
FengqiHu <louishurris@gmail.com>
2026-01-24 16:30:24 -0500

    Solve Q32, 80.53%, memory 63.55%

---
+++ src/StackSet.java
@@ -0,0 +1,45 @@
+import java.util.Stack;
+
+/**
+ * @author louishu
+ * @date 1/24/26 00:41
+ * @description
+ */
+public class StackSet {
+    /**
+     * 32. Longest Valid Parentheses - hard
+     * @Date - 01/24/2026
+     * @param s
+     * @return
+     */
+    public int longestValidParentheses(String s) {
+        Stack< Integer> stack = new Stack<>();
+        int res = 0;
+        // start represents the current valid start position
+        int start = -1;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c =='('){
+                // record the start position
+                stack.push(i);
+            }else{
+                // the split pos, mark as start
+                if(stack.isEmpty()){
+                    start = i;
+                }else{
+                    stack.pop();
+                    // completely valid - ()()()
+                    if (stack.isEmpty()){
+                        res = Math.max(res,i-start);
+                    }else{
+                        // possibly has other - (()())
+                        res = Math.max(res,i-stack.peek());
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+}
