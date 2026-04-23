a1be0ce5007e6ae79c1b250442fe10764ada02c8
FengqiHu <louishurris@gmail.com>
2026-04-23 15:17:19 -0400

    Q1143, 90.7% dp

--- src/DynamicPlan.java
+++ src/DynamicPlan.java
@@ -398,4 +398,38 @@ public class DynamicPlan {
         }
         return dp[m][n];
     }
+
+
+    /**
+     * 1143. Longest Common Subsequence
+     *
+     * @param text1
+     * @param text2
+     * @return
+     */
+    public int longestCommonSubsequence(String text1, String text2) {
+        int len1 = text1.length();
+        int len2 = text2.length();
+        int dp[][] = new int[len2 + 1][len1 + 1];
+//
+//        for (int i = 0; i <= len1; i++) {
+//            dp[0][i] = 0;
+//        }
+//        for (int i = 0; i <= len2; i++) {
+//            dp[i][0] = 0;
+//        }
+
+        for (int i = 1; i <= len2; i++) {
+            for (int j = 1; j <= len1; j++) {
+                if (text1.charAt(j - 1) != text2.charAt(i - 1)) {
+                    dp[i][j] = Math.max(dp[i][j - 1], dp[i - 1][j]);
+                } else {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+            }
+        }
+        return dp[len2][len1];
+
+    }
+
 }
