3b544d6be02a07f441853a39b7261178dcb2353b
FengqiHu <louishurris@gmail.com>
2026-04-25 15:47:27 -0400

    Q198, 100% dp

--- src/DynamicPlan.java
+++ src/DynamicPlan.java
@@ -15,14 +15,20 @@ public class DynamicPlan {
      */
     public static int rob(int[] nums) {
         int n = nums.length;
-        int memo[] = new int[n];
-        memo[0] = nums[0];
-        memo[1] = Math.max(nums[0], nums[1]);
-        for (int i = 2; i < n; i++) {
-            int tmp = nums[i] + nums[i - 2];
-            memo[i] = Math.max(memo[i - 1], tmp);
+        int dp[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = nums[i];
         }
-        return memo[n - 1];
+        if (n == 1)
+            return nums[0];
+        dp[1] = Math.max(nums[1], nums[0]);
+        for (int i = 2; i < n; i++) {
+            // rob: use the dp[i-2] + current value
+            // not rob: use the previous dp[i-1]
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[n - 1];
     }
 
     /**
